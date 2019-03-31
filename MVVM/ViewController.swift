033bb47afaffa9434b4dd80a2b3c363ff4b9cc8b
Daniel Demjen <demdani01@gmail.com>
2019-03-31 10:39:16 +0200

    Create project

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Demjén Dániel on 2019. 03. 31..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
